0a35163457b44de80dcb8e59d6d9f7c1bbdfe966
Luiz Fernando Mattos <luizfernandoamattos@gmail.com>
2022-08-31 12:26:12 -0300

    tela 2 quase finalizada

--- script/2-screen-interaction.js
+++ script/2-screen-interaction.js
@@ -1,25 +1,57 @@
-let mainElement = document.querySelector('.second-screen')
-let quizzHeader = document.querySelector('.quizz-header');
+const mainElement = document.querySelector('.second-screen')
+const section = document.querySelector('.second-screen section')
 
 
-function getQuizzes() {
-    const response = axios.get(url);
-    response.catch(()=>{
-        location.reload();
-    })
+const response = axios.get(`${url}/9969`);
+response.catch(()=>{
+    location.reload();
+})
 
-    response.then((quizzes)=>{
-        quizzHP = quizzes.data[3];
+response.then((response)=>{
+    const quizz = response.data;
+    console.log(quizz)
+    createHeaderHTML(quizz)
+    createQuizzAlternativesHTML(quizz)
+})
 
-        createHTML(quizzHP);
-    })
-
+function createHeaderHTML(quizz) {
+    let imageElement = document.createElement("img");
+    let headerImageContainer = document.querySelector('.bg-opacity')
+    let headerH1 = document.querySelector('.quizz-header h1');
     
+    headerH1.textContent = quizz.title;
+    imageElement.src = quizz.image;
+    headerImageContainer.appendChild(imageElement);
 }
 
-function createHTML(quizzHP) {
-    let imageElement = document.createElement("img");
-    imageElement.src = quizzHP.image;
-    quizzHeader.appendChild(imageElement);
+function createQuizzAlternativesHTML(quizz) {
+    for (let question of quizz.questions){
+        section.innerHTML +=
+        `
+        <div class="quizz-question-container">
+            <div class="quizz-question">
+                <h1>${question.title}</h1>
+            </div>
+            <div class="quizz-alternatives">
+            </div>
+        </div>
+        `
+        h1Container = document.querySelector('.quizz-question');
+        h1Container.backgroundColor = question.color;
+
+        let alternatives = document.querySelector('.quizz-alternatives');
+
+        for(answer of question.answers) {
+            let div = alternatives.appendChild(document.createElement('div'))
+            let img = div.appendChild(document.createElement('img'))
+            img.src = answer.image;
+            img.classList.add('quizz-alternative-image')
+            let text = div.appendChild(document.createElement('p'));
+            text.textContent = answer.text;
+        }
+
+
+        // section.appendChild()
+    }
 
 }
