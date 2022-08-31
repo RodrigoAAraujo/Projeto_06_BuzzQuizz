05e572e32a37623fe5a6933f7c82cf3c0b2e2c3b
Rodrigo Antunes Araujo <rodrigo.teth@gmail.com>
2022-08-31 17:50:18 -0300

    Comportamento da aba Meus Quizzes

--- script/1-screen-interaction.js
+++ script/1-screen-interaction.js
@@ -1,20 +1,53 @@
 const otherQuizzes = document.querySelector(".first-screen .all-quizzes ul")
 const myQuizzes = document.querySelector(".first-screen .my-quizzes ul")
+let myQuizzesCreated = []
+let alreadyHasQuiz = true
 
 
-const responseGetQuizzes = axios.get(`${url}`)
+function getAndRenderQuizzes(){
+    const responseGetQuizzes = axios.get(`${url}`)
+    responseGetQuizzes.then((response)=>{
+        response.data.forEach(element => {
+            let quizId = element.id
+            let quizImage = element.image
+            let quizTitle = element.title
 
-responseGetQuizzes.then((response)=>{
-    response.data.forEach(element => {
-        let quizId = element.id
-        let quizImage = element.image
-        let quizTitle = element.title
+            if (myQuizzesCreated.length == 0 || myQuizzesCreated.forEach((element) => element =! quizId)){
+                otherQuizzes.innerHTML += `
+                <li class="${quizId}" onclick="enterQuiz()">
+                    <h2>${quizTitle}</h2>
+                    <img src="${quizImage}"></img>
+                </li>
+                `
+            }else{
+                alreadyHasQuiz = true
+                myQuizzes.innerHTML += `
+                <li class="${quizId}" onclick="enterQuiz()">
+                    <h2>${quizTitle}</h2>
+                    <img src="${quizImage}"></img>
+                </li>
+                `
+            }
 
-        otherQuizzes.innerHTML += `
-        <li class="${quizId}">
-            <h2>${quizTitle}</h2>
-            <img src="${quizImage}">
-        </li>
-        `
-    });
-})
+            
+        });
+    })
+    responseGetQuizzes.catch((error)=>{
+        let status = error.response.data
+        alert(status)
+    })
+    HasQuizzes()
+}
+
+function HasQuizzes(){
+    const MyQuizDescription = document.querySelector(".first-screen .my-quizzes > div")
+    const noQuizPop = document.querySelector(".first-screen .my-quizzes .no-quiz")
+    if (alreadyHasQuiz == false){
+        MyQuizDescription.classList.add("hidden")
+        myQuizzes.classList.add("hidden")
+        noQuizPop.classList.remove("hidden")
+    }
+}
+
+
+getAndRenderQuizzes()
